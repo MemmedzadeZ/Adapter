bc8fd0f49fd71289c4641660a365cccd75838118
Zehra Memmedzade <zehra@gmail.com>
2023-12-02 23:51:22 +0400

    Добавьте файлы проекта.

---
+++ Adapter/Adapteer.cs
@@ -0,0 +1,64 @@
+﻿namespace Adapter;
+
+
+
+/*
+ Sadece maraqli bir numune getirmek isdedim 
+bir director interface yaradaraq her fennin  muellimini 
+oz icinde cigirdim.
+ ve daha sonra Adapter qaydasina reayet etdim
+ 
+ 
+ */
+interface IDirector
+{
+    void Subject();
+
+}
+
+
+class MathTeacher : IDirector
+{
+    public void Subject()
+    {
+        Console.WriteLine("Matematic ");
+
+
+    }
+}
+
+class HistoryTeacher : IDirector
+{
+    public void Subject()
+    {
+        Console.WriteLine("History ");
+    }
+}
+
+class GeographTeacher : IDirector
+{
+    public void Subject()
+    {
+        Console.WriteLine("GeoGraph");
+
+    }
+}
+
+class EnglishTeacher
+{ 
+
+    public void SubjectAudi()
+    {
+        Console.WriteLine("English ");
+    }
+}
+
+//Class Adapter
+class EnglishAdapter : EnglishTeacher, IDirector
+{
+    public void Subject()
+    {
+        SubjectAudi();
+    }
+}
+
---
+++ Adapter/Program.cs
@@ -0,0 +1,18 @@
+﻿using Adapter;
+using System.Xml;
+
+List<IDirector> directors = new()
+{
+    new MathTeacher(),
+    new HistoryTeacher(),
+    new HistoryTeacher(),
+    new GeographTeacher(),
+   new EnglishAdapter()
+
+
+};
+
+foreach(var director in directors)
+{
+    director.Subject();
+}
